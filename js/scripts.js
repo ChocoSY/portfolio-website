0e1cac46fea5bc81d8c22e39882f9ea86d42a6bf
ChocoSY <105742562+ChocoSY@users.noreply.github.com>
2022-06-19 23:36:05 -0700

    Toggle on/off for menu box_js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
-console.log("Hello! Welcome to my profile site!");
-alert("Hi! Welcome to my Portfolio Site!");
+// console.log("Hello! Welcome to my profile site!");
+// alert("Hi! Welcome to my Portfolio Site!");
 
+function menuToggle() {
+    //Searches the doc for the element with given ID
+    var x = document.getElementById('myNavtoggle');
+
+    //Change the class of the object based on its status (open/closed)
+    if (x.className === 'navtoggle') {
+        //this line shows the menu (Add class name ' responsive')
+        x.className += ' responsive'; //Need A SPACE before responsive for successful execute!
+        //??? is it adding the name as a separate class or it is just changing the name of the class by adding this word at the end???
+    } else { //in case of class name is both 'navtoggle' and ' reponsive'
+        x.className = 'navtoggle'; //effectively hide menu
+    }
+    
+}
